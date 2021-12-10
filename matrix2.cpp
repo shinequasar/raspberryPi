d80d2b5e6fc861dc606d4b51e3909dbecc8231be
shinequasar <shinequasar@gmail.com>
2021-12-10 14:15:17 +0900

    add

--- matrix2.cpp
+++ matrix2.cpp
@@ -64,13 +64,13 @@ int main(int argc, char** argv)
     // send_MAX7219(8, digit_table[0], dot_table[1][8]);
  
     send_MAX7219(1, digit_table[0]);
-    send_MAX7219(2, digit_table[0]);
-    send_MAX7219(3, digit_table[0]);
-    send_MAX7219(4, digit_table[0]);
-    send_MAX7219(5, digit_table[0]);
-    send_MAX7219(6, digit_table[0]);
-    send_MAX7219(7, digit_table[0]);
-    send_MAX7219(8, digit_table[0]);
+    // send_MAX7219(2, digit_table[1]);
+    // send_MAX7219(3, digit_table[2]);
+    // send_MAX7219(4, digit_table[3]);
+    // send_MAX7219(5, digit_table[4]);
+    // send_MAX7219(6, digit_table[5]);
+    // send_MAX7219(7, digit_table[6]);
+    // send_MAX7219(8, digit_table[7]);
 
 
     //intHandler(0);
